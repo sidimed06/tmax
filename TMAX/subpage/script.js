b8e44d5496d8fcc9e000303d40352e2100738ef8
sidimed06 <160360149+sidimed06@users.noreply.github.com>
2024-02-17 22:10:18 +0100

    Add files via upload

---
+++ TMAX/subpage/script.js
@@ -0,0 +1,16 @@
+let menu= document.querySelector('#menu-icon');
+let navlist= document.querySelector('.navlist');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset:true
+});
+sr.reveal('h1',{delay:200, origin :'top'});
+sr.reveal('.tmax1-text',{delay:400, origin :'top'});
+sr.reveal('.Telephone',{delay:400, origin :'top'});
+sr.reveal('.icons',{delay:500, origin :'left'});
